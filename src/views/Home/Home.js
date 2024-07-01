cc5f50e5c4413ea69027bf3cefd49ebc15d2fb79
athav2823 <atharvaneware@gmail.com>
2024-07-01 22:18:26 +0530

    alert is mofdified

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -3,6 +3,7 @@ import send from "./send.png"
 import TodoCards from "./../../components/TodoCards/TodoCards"
 import { useState, useEffect } from "react"
 import toast, {Toaster} from "react-hot-toast"
+import Swal from "sweetalert2"
 
 function Home() {
 
@@ -12,6 +13,59 @@ function Home() {
     const [newTask ,SetNewTask] = useState ("")
 
     const [categories , Setcategories] = useState ("")
+
+    useEffect(() => {
+        const savedList = localStorage.getItem("todoList")
+
+        if (savedList) {
+            setTodoList(JSON.parse(savedList))
+        }
+    }, [])
+
+    useEffect(() => {
+        if (todoList.length === 0) return
+
+        localStorage.setItem("todoList", JSON.stringify(todoList))
+    }, [todoList])
+
+    function deleteItem(index) {
+
+        Swal.fire({
+            title: "Are You Sure ?",
+            text: "You Want To Delete This Item ?",
+            icon: "warning",
+            showCancelButton: true,
+        }).then((result) => {
+
+            if (!result.isConfirmed) {
+               return
+            }
+
+            else{
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                  });
+            }
+
+            const newList = todoList.filter((item, i) => {
+                if (i != index) {
+                    return true
+                }
+    
+                else {
+                    return false
+                }
+            })
+            setTodoList(newList)
+
+            
+        })
+
+        
+    }
+
 
 
     return (
@@ -63,6 +117,11 @@ function Home() {
                         if (newTask=== ""){
                             toast.error("Please Enter a Task")
                             return
+                        }
+                        if (category === "") {
+                            toast.error("Category Cannot be empty...")
+                            return
+
                         }
 
 
